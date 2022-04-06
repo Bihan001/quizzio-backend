275a1e21cde6f6afe783923d9df73d2768511e9d
Bihan Chakraborty <bhchakraborty001@gmail.com>
2022-04-06 14:50:23 +0530

    [Added] Prisma Client Connection

---
+++ src/database/prisma-client.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient({
+  errorFormat: 'pretty',
+  log: [
+    {
+      emit: 'event',
+      level: 'query',
+    },
+    {
+      emit: 'stdout',
+      level: 'error',
+    },
+    {
+      emit: 'stdout',
+      level: 'info',
+    },
+    {
+      emit: 'stdout',
+      level: 'warn',
+    },
+  ],
+});
+
+prisma.$on('query', (e) => {
+  console.log('Query: ' + e.query);
+  console.log('Params: ' + e.params);
+  console.log('Duration: ' + e.duration + 'ms');
+});
+
+export default prisma;
