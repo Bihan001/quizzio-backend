96bc2fb09bf60517c52afdde67a292a8ba2bc155
Bihan Chakraborty <bhchakraborty001@gmail.com>
2023-05-02 22:05:40 +0530

    removed dist

--- dist/utils/catchAsync.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = (fn) => {
-    return (req, res, next) => {
-        fn(req, res, next).catch((err) => next(err));
-    };
-};
